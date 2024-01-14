2b8551e41c050e1fd98dcee11afde362223a10b3
g2Min <jimin9320@gmail.com>
2024-01-14 23:07:48 +0900

    refactor: 불필요한 코드 삭제

--- src/AGVSim.java
+++ src/AGVSim.java
@@ -16,8 +16,6 @@ public class AGVSim {
 			// 시뮬레이션 실행 (예: 100분)
 			engine.run();
 
-			// debugging이 잡히면 inventory에 들어간 부품 개수만큼 엔터 시키는 것
-			// 이 부분은 시뮬레이션 로그에 대한 추가적인 설명이 필요합니다.
 		} catch (IllegalArgumentException e) {
 			System.err.println("Error: " + e.getMessage());
 		} catch (FileNotFoundException e) {
